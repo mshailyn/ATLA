f04b1b362ce7525b75da08b9e0a6bdbaa96a83cb
Shailyn Moore <m.shailyn@yahoo.com>
2022-10-11 18:18:28 -0700

    update CSS and added music

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # ATLA
-This is a small project for an Avatar the Last Airbender Database using information form an API. 
+This is a small project for an Avatar the Last Airbender Database using information from an API. 
 
 **Link to project:** https://atla-db.netlify.app/
 
-![alt tag](img/homepage.png)
+![Picture of ATLA Database](img/homepage.png)
 
 ## How It's Made:
 
@@ -16,13 +16,13 @@ This porject is mainly javascript with some rough HTML and CSS. This project is 
 Fixing the HTML and CSS to make this project look cleaner would definitely elevate this application. I think also connecting to other APIs would allow this project to present more information that I couldn't get from just a singular API (like images of the characters). 
 ## Lessons Learned:
 
-The big thing I learned from this project is how to use a fetch to receive information from an API and how to sort through the JSON to pick out what I needed and show that on my application.
+The big thing I learned from this project is how to use a fetch to receive information from a REST API and how to sort through the JSON to pick out what I needed and show those responses on my application.
 
 ## Examples:
 Take a look at these couple examples that I have in my own portfolio:
 
-**Studio Ghibli Database:** https://github.com/mshailyn/StudioGhibli
+**Dandelion:** https://github.com/mshailyn/100Hours
 
 **Helio Salon:** https://github.com/mshailyn/heliosalon
 
-**CodeWars Repo** https://github.com/mshailyn/CodeWars
+**Binary Upload Boom** https://github.com/mshailyn/social-media-app
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+document.querySelector('body').addEventListener('mouseover', playAudioStart)
+document.querySelector('a').addEventListener('click', muteOrPlay)
+const muteButton = document.getElementById('mute')
+
+function muteOrPlay(){
+  if(muteButton.classList.contains('fa-volume-high')){
+    muteButton.classList.toggle('fa-volume-high')
+    muteButton.classList.add('fa-volume-xmark')
+    muteAudio()
+  }else{
+    muteButton.classList.toggle('fa-volume-xmark')
+    muteButton.classList.add('fa-volume-high')
+    playAudio
+  }
+}
+
+function playAudioStart(){
+  if (muteButton.classList.contains('fa-volume-high')){
+  const audio = document.getElementById('audioelement')
+  audio.volume = 0.1;
+  audio.play()
+  }
+}
+
+function playAudio(){
+  const audio = document.getElementById('audioelement')
+  audio.volume = 0.1;
+  audio.play()
+
+}
+
+function muteAudio(){
+  const audio = document.getElementById('audioelement')
+  audio.pause()
+}
+
 //Example fetch
 document.querySelector('button').addEventListener('click', getFetch)
 
@@ -11,14 +47,14 @@ function getFetch(){
       .then(data => {
         console.log(data)
         if(data[0].name.includes('(') && !data[1].name.includes('(')) {
-          document.querySelector('h2').innerText = data[1].name
+          document.querySelector('#name').innerText = data[1].name
           document.querySelector('img').src = data[1].photoUrl
           document.querySelector('#ally').innerText = data[1].allies
           document.querySelector('#enemy').innerText = data[1].enemies
           document.querySelector('#affiliation').innerText = data[1].affiliation
         }
         else{
-        document.querySelector('h2').innerText = data[0].name
+        document.querySelector('#name').innerText = data[0].name
         document.querySelector('img').src = data[0].photoUrl
         document.querySelector('#ally').innerText = data[0].allies
         document.querySelector('#enemy').innerText = data[0].enemies
